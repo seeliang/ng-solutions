2f693aa931dc3a35b34024389bd28f2f060ec3ba
sihui liang <sihuiliang@MacBook-Pro.local>
2020-06-25 10:16:33 +1000

    initial grid

--- ag-grid/src/app/app.component.ts
+++ ag-grid/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {GridOptions} from "ag-grid-community";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ag-grid';
+    public rowData:any[];
+    public columnDefs:any[];
+
+    constructor() {
+
+        this.columnDefs = [
+            {headerName: "Make", field: "make"},
+            {headerName: "Model", field: "model"},
+            {headerName: "Price", field: "price"}
+        ];
+        this.rowData = [
+            {make: "Toyota", model: "Celica", price: 35000},
+            {make: "Ford", model: "Mondeo", price: 32000},
+            {make: "Porsche", model: "Boxter", price: 72000}
+        ];
+    }
 }
--- ag-grid/src/app/app.module.ts
+++ ag-grid/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AgGridModule} from "ag-grid-angular";
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule.withComponents([]
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
