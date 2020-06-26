626a74d879848f860726cb25ce05343762695c45
sihui liang <sihuiliang@MacBook-Pro.local>
2020-06-26 16:41:49 +1000

    feat: move data to container

--- ag-grid/packages/molecules/table/index.ts
+++ ag-grid/packages/molecules/table/index.ts
@@ -1,24 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
     selector: 'm-table',
     templateUrl: './table.html',
   })
 
 export class Table {
+    @Input() 
+    tableData
+
     public rowData:any[];
     public columnDefs:any[];
 
-    constructor() {
+    ngOnInit() {
+        this.readData();
+    }
 
-        this.columnDefs = [
-            {headerName: "Make", field: "make"},
-            {headerName: "Model", field: "model"},
-            {headerName: "Price", field: "price"}
-        ];
-        this.rowData = [
-            {make: "Toyota", model: "Celica", price: 35000},
-            {make: "Ford", model: "Mondeo", price: 32000},
-            {make: "Porsche", model: "Boxter", price: 72000}
-        ];
+    readData() {
+        const {columnDefs, rowData} = this.tableData;
+        this.columnDefs = columnDefs;
+        this.rowData = rowData;
     }
 }
--- ag-grid/src/app/app.component.ts
+++ ag-grid/src/app/app.component.ts
@@ -1,27 +1,32 @@
 import { Component } from '@angular/core';
-import {GridOptions} from "ag-grid-community";
+const sampleA = {
+  columnDefs: [
+    {headerName: "Make", field: "make"},
+    {headerName: "Model", field: "model"},
+    {headerName: "Price", field: "price"}
+],
+rowData : [
+    {make: "Toyota", model: "Celica", price: 35000},
+    {make: "Ford", model: "Mondeo", price: 32000},
+    {make: "Porsche", model: "Boxter", price: 72000}
+]
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'ag-grid';
-    public rowData:any[];
-    public columnDefs:any[];
+    public tableData: object;
 
     constructor() {
+      this.tableData = {}
+    }
 
-        this.columnDefs = [
-            {headerName: "Make", field: "make"},
-            {headerName: "Model", field: "model"},
-            {headerName: "Price", field: "price"}
-        ];
-        this.rowData = [
-            {make: "Toyota", model: "Celica", price: 35000},
-            {make: "Ford", model: "Mondeo", price: 32000},
-            {make: "Porsche", model: "Boxter", price: 72000}
-        ];
+    ngOnInit() {
+      this.tableData= sampleA
     }
 }
