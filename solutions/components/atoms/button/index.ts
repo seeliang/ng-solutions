ff549dddb57303e6156a807f2214f5db5b5e163e
sihui liang <sihuiliang@MacBook-Pro.local>
2020-07-09 15:48:08 +1000

    chore: break code

--- solutions/components/atoms/button/index.ts
+++ solutions/components/atoms/button/index.ts
@@ -2,22 +2,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'storybook-button-component',
-  template: `
-    <button (click)="onClick.emit($event)">{{ text }}</button>
-  `,
-  styles: [
-    `
-      button {
-        border: 1px solid #eee;
-        border-radius: 3px;
-        background-color: #ffffff;
-        cursor: pointer;
-        font-size: 15px;
-        padding: 3px 10px;
-        margin: 10px;
-      }
-    `,
-  ],
+  templateUrl: './button.html',
+  styleUrls: ['./button.scss'],
 })
 export class Button {
   @Input()
@@ -25,4 +11,8 @@ export class Button {
 
   @Output()
   onClick = new EventEmitter<any>();
+
+  ngOnInit() {
+    console.log(322243e3334)
+  }
 }
