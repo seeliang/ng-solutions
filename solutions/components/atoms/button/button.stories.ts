1822f5119282f0084a7fbfddef26f2e85fe57374
sihui liang <sihuiliang@MacBook-Pro.local>
2020-07-09 15:31:25 +1000

    fix: correct the import

--- solutions/components/atoms/button/button.stories.ts
+++ solutions/components/atoms/button/button.stories.ts
@@ -1,7 +1,7 @@
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button } from '@storybook/angular/demo';
+import { Button } from './index';
 
 export default {
   title: 'Button',
