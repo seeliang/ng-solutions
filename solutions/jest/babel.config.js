0b20cfbcc3d93511ae4ed98fda2730e38c1728be
sihui liang <sihuiliang@MacBook-Pro.local>
2020-07-13 11:48:18 +1000

    chore: add decorators support

--- solutions/jest/babel.config.js
+++ solutions/jest/babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  "plugins": [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose" : true }]
+  ],
   presets: [
     '@babel/preset-typescript',
     [
