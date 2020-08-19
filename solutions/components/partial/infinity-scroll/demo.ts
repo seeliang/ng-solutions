f75fe0b2eb4735425b37999e35836af93e53242d
seeliang <hi@sihuiliang.com>
2020-08-19 11:04:58 +1000

    HOC sample

---
+++ solutions/components/partial/infinity-scroll/demo.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'infinity-scroll-demo',
+  templateUrl: './demo.html',
+})
+
+export class InfinityScrollDemo {
+  ngAfterViewInit() {
+    console.log('34')
+  }
+}
--- solutions/components/partial/infinity-scroll/infinity-scroll.stories.ts
+++ solutions/components/partial/infinity-scroll/infinity-scroll.stories.ts
@@ -1,10 +1,18 @@
-import { InfinityScroll } from './index';
+import { InfinityScrollDemo } from './demo';
+import { CommonModule } from '@angular/common'
+import { moduleMetadata } from '@storybook/angular';
+import {InfinityScroll} from './index'
 
 export default {
   title: 'InfinityScroll',
-  component: InfinityScroll,
+  decorators: [
+    moduleMetadata({
+      declarations: [InfinityScrollDemo, InfinityScroll],
+      imports: [CommonModule],
+    }),
+  ],
 };
 
-export const hi = () => ({
-  component: InfinityScroll,
+export const Sample = () => ({
+  component: InfinityScrollDemo,
 });
