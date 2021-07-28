6013b23adea1841ba5ec84d2f49f347520ee7ab9
seeliang <hi@sihuiliang.com>
2021-07-29 09:02:07 +1000

    init ie

--- solutions/ie11/karma.conf.js
+++ solutions/ie11/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ie11'),
+      dir: require('path').join(__dirname, './coverage/ie'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
