bac8c22dcf9847e9ec0da1a19f34f52b782891c2
seeliang <hi@sihuiliang.com>
2021-07-30 09:58:34 +1000

    chore: performance boost add route reuse

--- solutions/ie11/src/app/app-routing.module.ts
+++ solutions/ie11/src/app/app-routing.module.ts
@@ -4,6 +4,37 @@ import { NotFoundComponent } from './not-found/not-found.component'
 import {MainComponent} from './main/main.component'
 import { HitComponent} from './hit/hit.component'
 
+import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy } from '@angular/router';
+
+export class setRouteReuseStrategy implements RouteReuseStrategy {
+  handlers: { [key: string]: DetachedRouteHandle } = {};
+
+  shouldDetach(route: ActivatedRouteSnapshot): boolean {
+    return true;
+  }
+
+  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+    this.handlers[route.routeConfig.path] = handle;
+  }
+
+  shouldAttach(route: ActivatedRouteSnapshot): boolean {
+    return !!route.routeConfig && !!this.handlers[route.routeConfig.path];
+  }
+
+  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
+    if (!route.routeConfig) {
+      return null;
+    }
+
+    return this.handlers[route.routeConfig.path];
+  }
+
+  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+
+    return future.routeConfig === curr.routeConfig;
+  }
+}
+
 
 const routes: Routes = [
   {
@@ -22,6 +53,14 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: setRouteReuseStrategy
+    }
+  ]
 })
 export class AppRoutingModule { }
+
+
